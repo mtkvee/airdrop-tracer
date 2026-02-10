9394548cde128431c8ff45f83d0909d9de60180e
mtkvee <myotintkyaw.general@gmail.com>
2026-02-10 23:41:35 +0630

    fix CSP error

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
       "img-src 'self' data: blob: https: https://*.googleusercontent.com",
       "font-src 'self' data: https://fonts.gstatic.com https://cdnjs.cloudflare.com",
       "connect-src 'self' https://*.googleapis.com https://apis.google.com https://accounts.google.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com https://*.gstatic.com",
-      "frame-src 'self' https://accounts.google.com https://*.google.com",
+      "frame-src 'self' https://accounts.google.com https://*.google.com https://*.firebaseapp.com",
       "upgrade-insecure-requests",
     ].join('; ');
 
@@ -28,6 +28,7 @@ const nextConfig = {
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
         ],
       },
